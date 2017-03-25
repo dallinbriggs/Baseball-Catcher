8262f3605741beffee1285fec138a2cc2e039b8f
Dallin Briggs <dallinbriggs@gmail.com>
2017-03-25 14:32:26 -0600

    added image reading to main

--- QT/main.cpp
+++ QT/main.cpp
@@ -17,6 +17,16 @@ int main(int argc, char *argv[])
     Mat cameraMatrix_left;
     Mat cameraMatrix_right;
 
+    Mat image_left;
+    Mat image_right;
+    Mat image_left_color;
+    Mat image_right_color;
+
+    string header;
+    string tail;
+    string filename_left;
+    string filename_right;
+
 
     FileStorage fs_left("c:\\Projects\\BaseballCatcher - 10\\calibration\\calib_left.yaml", FileStorage::READ);
     fs_left["CameraMatrix"] >> cameraMatrix_left;
@@ -31,7 +41,25 @@ int main(int argc, char *argv[])
     fs_rectify["P2"] >> P2;
     fs_rectify["Q"] >> Q;
 
+    header = "/home/dallin/robotic_vision/HW4/ImagesDallin/Ball_test";
+    tail = ".bmp";
 
+    for(int i = 1; i < 100; i++)
+    {
+        filename_left = header + "L" + to_string(i) + tail;
+        filename_right = header + "R" + to_string(i) + tail;
+
+        image_left = imread(filename_left,CV_LOAD_IMAGE_GRAYSCALE);
+        image_right = imread(filename_right,CV_LOAD_IMAGE_GRAYSCALE);
+
+        imshow("Left", image_left);
+        imshow("right", image_right);
+        moveWindow("right",643,23);
+        waitKey(50);
+
+    }
+
+    waitKey(0);
 
     return 0;
 }
